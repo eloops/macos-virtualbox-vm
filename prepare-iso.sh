0cfb0889dfaefbd4c8e63290a0c0469303323df6
Steve P <eloops@users.noreply.github.com>
2020-04-08 23:44:23 +1000

    Global vars, fixed Mojave build commands, extra notes
    Added global vars to fix the if/elif tree checking.
    Fixed the commands for creating the Mojave image (macOS Base System vs OS X Base System)
    Added a bunch of notes for prepping a stub downloaded installation.

--- prepare-iso.sh
+++ prepare-iso.sh
@@ -8,7 +8,42 @@
 #   - High Sierra (10.13)
 #   - Mojave (10.14)
 
+# Prep 
+# Step 1: 
+# if you have downloaded a stub installer
+# Use disk utility to create a temp partition 25 Gb in size and install to that (but don't reboot).
+# then in terminal:
+# sudo -s
+# mkdir /Applications/Install\ macOS\ Mojave.app/Contents/SharedSupport
+# cd /Volumes/{name of partition}/macOS\ Install\ Data/
+# find . mount | cpio -pvdm /Applications/Install\ macOS\ Mojave.app/Contents/SharedSupport
+#
+# Ref: https://techsviewer.com/how-to-download-full-macos-catalina-installer-and-mojave/
+
+# Prep 2:
+# If there is a file InstallESDDmg.pkg in /Applications/Install\ macOS\ Mojave.app/Contents/SharedSupport
+# Rename it to InstallESD.dmg
+# nano /Applications/Install\ macOS\ Mojave.app/Contents/SharedSupport/InstallInfo.plist
+# take out the 4 lines under Payload Image Info that mention chunklist:
+#   <key>chunklistURL</key>
+#   <string>InstallESDDmg.chunklist</string>
+#   <key>chunklist.id</key>
+#   <string>com.apple.chunklist.InstallESDDmg</string>
+# Edit the lines above and below that section to correct the name of InstallESD.dmg:
+#   <string>InstallESDDmg.pkg</string> --> <string>InstallESD.dmg</string>
+#   ...
+#   <string>com.apple.pkg.InstallESDDmg</string> --> <string>com.apple.pkg.InstallESD</string>
+# Save and exit.
+# Now run this script.
+#
+# Ref: https://www.insanelymac.com/forum/topic/329828-making-a-bootable-high-sierra-usb-installer-entirely-from-scratch-in-windows-or-linux-mint-without-access-to-mac-or-app-store-installerapp/
+
 set -e
+
+#global vars because bash sucks
+installerAppName=0
+result=0
+error=0
 
 #
 # createISO
@@ -19,9 +54,9 @@ set -e
 function createISO()
 {
   if [ $# -eq 2 ] ; then
-    local installerAppName=${1}
+    installerAppName=${1}
     local isoName=${2}
-    local error=0
+    error=0
 
     # echo Debug: installerAppName = ${installerAppName} , isoName = ${isoName}
 
@@ -45,7 +80,7 @@ function createISO()
 
     if [ ${error} -ne 0 ] ; then
       echo "Failed to mount the InstallESD.dmg from the instaler at ${installerAppName}.  Exiting. (${error})"
-      return ${error}
+      exit $error
     fi
 
     echo
@@ -74,9 +109,12 @@ function createISO()
     echo
     echo Remove Package link and replace with actual files
     echo --------------------------------------------------------------------------
-    if [ "${isoName}" == "HighSierra" ] || [ "${isoName}" == "Mojave" ] ; then
+    if [ "${isoName}" == "HighSierra" ] ; then
       echo $ ditto -V /Volumes/install_app/Packages /Volumes/OS\ X\ Base\ System/System/Installation/
       ditto -V /Volumes/install_app/Packages /Volumes/OS\ X\ Base\ System/System/Installation/
+    elif [ "${isoName}" == "Mojave" ] ; then
+      echo $ ditto -V /Volumes/install_app/Packages /Volumes/macOS\ Base\ System/System/Installation/
+      ditto -V /Volumes/install_app/Packages /Volumes/macOS\ Base\ System/System/Installation/
     else
       echo $ rm /Volumes/OS\ X\ Base\ System/System/Installation/Packages
       rm /Volumes/OS\ X\ Base\ System/System/Installation/Packages
@@ -87,11 +125,16 @@ function createISO()
     echo
     echo Copy macOS ${isoName} installer dependencies
     echo --------------------------------------------------------------------------
-    if [ "${isoName}" == "HighSierra" ] || [ "${isoName}" == "Mojave" ] ; then
+    if [ "${isoName}" == "HighSierra" ] ; then
       echo $ ditto -V "${installerAppName}"/Contents/SharedSupport/BaseSystem.chunklist /Volumes/OS\ X\ Base\ System/BaseSystem.chunklist
       ditto -V "${installerAppName}"/Contents/SharedSupport/BaseSystem.chunklist /Volumes/OS\ X\ Base\ System/BaseSystem.chunklist
       echo $ ditto -V "${installerAppName}"/Contents/SharedSupport/BaseSystem.dmg /Volumes/OS\ X\ Base\ System/BaseSystem.dmg
       ditto -V "${installerAppName}"/Contents/SharedSupport/BaseSystem.dmg /Volumes/OS\ X\ Base\ System/BaseSystem.dmg
+    elif [ "${isoName}" == "Mojave" ] ; then
+      echo $ ditto -V "${installerAppName}"/Contents/SharedSupport/BaseSystem.chunklist /Volumes/macOS\ Base\ System/BaseSystem.chunklist
+      ditto -V "${installerAppName}"/Contents/SharedSupport/BaseSystem.chunklist /Volumes/macOS\ Base\ System/BaseSystem.chunklist
+      echo $ ditto -V "${installerAppName}"/Contents/SharedSupport/BaseSystem.dmg /Volumes/macOS\ Base\ System/BaseSystem.dmg
+      ditto -V "${installerAppName}"/Contents/SharedSupport/BaseSystem.dmg /Volumes/macOS\ Base\ System/BaseSystem.dmg
     else
       echo $ cp -rp /Volumes/install_app/BaseSystem.chunklist /Volumes/OS\ X\ Base\ System/BaseSystem.chunklist
       cp -rp /Volumes/install_app/BaseSystem.chunklist /Volumes/OS\ X\ Base\ System/BaseSystem.chunklist
@@ -108,8 +151,13 @@ function createISO()
     echo
     echo Unmount the sparse bundle
     echo --------------------------------------------------------------------------
-    echo $ hdiutil detach /Volumes/OS\ X\ Base\ System/
-    hdiutil detach /Volumes/OS\ X\ Base\ System/
+    if [ "${isoName}" == "Mojave" ] ; then
+      echo $ hdiutil detach /Volumes/macOS\ Base\ System/
+      hdiutil detach /Volumes/macOS\ Base\ System/
+    else
+      echo $ hdiutil detach /Volumes/OS\ X\ Base\ System/
+      hdiutil detach /Volumes/OS\ X\ Base\ System/
+    fi
 
     echo
     echo Resize the partition in the sparse bundle to remove any free space
@@ -144,14 +192,14 @@ function createISO()
 #
 function installerExists()
 {
-  local installerAppName=$1
-  local result=1
+  installerAppName=$1
+  result=1
   if [ -e "${installerAppName}" ] ; then
     result=0
   elif [ -e /Applications/"${installerAppName}" ] ; then
     result=0
   fi
-  return ${result}
+  #return ${result}
 }
 
 #
@@ -165,28 +213,32 @@ done
 # See if we can find an eligible installer.
 # If successful, then create the iso file from the installer.
 installerExists "Install macOS Mojave.app"
-result=$?
-if [ ${result} -eq 0 ] ; then
+# result=$?
+if [[ ${result} -eq 0 ]]; then
+  echo ">>> Creating ISO for Mojave"
   createISO "Install macOS Mojave.app" "Mojave"
 else
+  echo ">>> No Mojave image found, trying High Sierra"
   installerExists "Install macOS High Sierra.app"
-  result=$?
-  if [ ${result} -eq 0 ] ; then
+  # result=$?
+  if [[ ${result} -eq 0 ]]; then
+    echo "Creating ISO for High Sierra"
     createISO "Install macOS High Sierra.app" "HighSierra"
   else
+    echo ">>> No High Sierra image found, trying Sierra"
     installerExists "Install macOS Sierra.app"
-    result=$?
-    if [ ${result} -eq 0 ] ; then
+    # result=$?
+    if [[ ${result} -eq 0 ]]; then
       createISO "Install macOS Sierra.app" "Sierra"
     else
       installerExists "Install OS X El Capitan.app"
-      result=$?
-      if [ ${result} -eq 0 ] ; then
+      # result=$?
+      if [[ ${result} -eq 0 ]]; then
         createISO "Install OS X El Capitan.app" "ElCapitan"
       else
         installerExists "Install OS X Yosemite.app"
-        result=$?
-        if [ ${result} -eq 0 ] ; then
+        # result=$?
+        if [[ ${result} -eq 0 ]]; then
           createISO "Install OS X Yosemite.app" "Yosemite"
         else
           echo "Could not find installer for Yosemite (10.10), El Capitan (10.11), Sierra (10.12), High Sierra (10.13) or Mojave (10.14)."
@@ -195,3 +247,4 @@ else
     fi
   fi
 fi
+
